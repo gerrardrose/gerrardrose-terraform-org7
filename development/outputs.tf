0c351455480336b24170a9079c568eb9bd76fa6f
Gerrard Rose <gerrard.rose@rebura.com>
2023-03-18 20:04:47 +0000

    test 14

---
+++ development/outputs.tf
@@ -0,0 +1,31 @@
+output "vpc_id" {
+  description = "ID of the vpc"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = module.subnets.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = module.subnets.private_subnet_ids
+}
+
+#output "instance_id" {
+#  description = "ID of EC2 instance"
+#  value       = module.instance.id
+#}
+
+#output "instance_public_ip" {
+#   description = "Public IP of EC2 instance"
+#   value       = module.instance.public_ip
+#}
+
+#output "RDS_endpoint" {
+#   description = "instance_endpoint of RDS instance"
+#   value       = module.rds_instance.instance_endpoint
+#}
+
+
