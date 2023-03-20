07bc5c1cfe9700d23078585a85b7fd593a1bfc1a
Gerrard Rose <gerrard.rose@rebura.com>
2023-03-20 21:17:59 +0000

    test 38

--- management/organization.tf
+++ management/organization.tf
@@ -1,4 +1,28 @@
+resource "aws_organizations_account" "management" {
+  name  = "Management"
+  email = "gez.rose+management.org.7@gmail.com"
+}
 resource "aws_organizations_account" "security" {
-  email = "gez.rose+security.org.7@gmail.com"
   name  = "Security"
+  email = "gez.rose+security.org.7@gmail.com"
+}
+resource "aws_organizations_account" "log-archive" {
+  name  = "Log Archive"
+  email = "gez.rose+log-archive.org.7@gmail.com"
+}
+resource "aws_organizations_account" "build" {
+  name  = "Build"
+  email = "gez.rose+build.org.7@gmail.com"
+}
+resource "aws_organizations_account" "development" {
+  name  = "Development"
+  email = "gez.rose+development.org.7@gmail.com"
+}
+resource "aws_organizations_account" "staging" {
+  name  = "Staging"
+  email = "gez.rose+staging.org.7@gmail.com"
+}
+resource "aws_organizations_account" "production" {
+  name  = "Production"
+  email = "gez.rose+production.org.7@gmail.com"
 }
